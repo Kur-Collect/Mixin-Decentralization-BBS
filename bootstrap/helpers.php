93ea0310a916761a0ecf1672ff2bb858a42fe6b9
Amatist_Kurisu <misaki.zhcy@gmail.com>
2018-12-16 21:39:38 +0800

    update

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -12,6 +12,23 @@ if (!function_exists('fetchMixinSDk')) {
         $rawKeys = Storage::get('MixinMatchBBS.keys');
         $keysArr = explode("\n", $rawKeys, 7);
         $keys    = array_combine(['mixin_id', 'client_id', 'client_secret', 'pin', 'pin_token', 'session_id', 'private_key'], $keysArr);
-        return \ExinOne\MixinSDK\Facades\MixinSDK::use('default', $keys);
+        return \ExinOne\MixinSDK\Facades\MixinSDK::use('default', getRobotConfig());
+    }
+}
+
+
+if (!function_exists('getRobotConfig')) {
+    function getRobotConfig()
+    {
+        $cacheKey = 'MixinMatchBBS.config';
+        $keyFile  = 'MixinMatchBBS.keys';
+        if (\Illuminate\Support\Facades\Cache::has($cacheKey)) {
+            return \Illuminate\Support\Facades\Cache::get($cacheKey);
+        } else {
+            $rawKeys = Storage::get($keyFile);
+            $keysArr = explode("\n", $rawKeys, 7);
+            $keys    = array_combine(['mixin_id', 'client_id', 'client_secret', 'pin', 'pin_token', 'session_id', 'private_key'], $keysArr);
+            return $keys;
+        }
     }
 }
